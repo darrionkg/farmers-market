12201944a8b1f7ec4c33d2f56cc26e8bbcd37b51
Student <example@example.com>
2019-06-19 11:17:20 -0700

    add bootstrap

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,28 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Farmer } from './../farmer/farmer.model';
-// import { Router } from '@angular/router';
-// import { FarmerService } from './../farmer.service';
-// import { FirebaseListObservable } from 'angularfire2/database';
-
-// @Component({
-//   selector: 'app-marketplace',
-//   templateUrl: './marketplace.component.html',
-//   styleUrls: ['./marketplace.component.css'],
-//   providers: [FarmerService]
-// })
-// export class MarketplaceComponent implements OnInit {
-//   farmers: FirebaseListObservable<any[]>;
-//   constructor(private router: Router, private farmerService: FarmerService){}
-
-//   ngOnInit() {
-//     this.farmers = this.farmerService.getFarmers();
-//   }
-
-//   goToDetailPage(clickedFarmer: Farmer) {
-//     this.router.navigate(['farmers', clickedFarmer.$key]);
-//   };
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Farmer } from '../farmer/farmer.model';
